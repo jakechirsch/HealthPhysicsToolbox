5989004c936b53c16218d078052eba4c9602d322
Jake Hirsch <jakechirsch@gmail.com>
2025-07-16 15:28:13 -0400

    Info tag fix

--- App/style.py
+++ App/style.py
@@ -138,14 +138,25 @@ class Tooltip:
         self.widget = widget
         self.text = text
         self.tooltip_window = None
-        widget.bind("<Enter>", self.show_tooltip)
-        widget.bind("<Leave>", self.hide_tooltip)
+        self.after_id = None
+        self.delay = 10
+        widget.bind("<Enter>", self.schedule_tooltip)
+        widget.bind("<Leave>", self.cancel_tooltip)
 
-    def show_tooltip(self, _):
+    def schedule_tooltip(self, _):
+        self.after_id = self.widget.after(self.delay, self.show_tooltip)
+
+    def cancel_tooltip(self, _):
+        if self.after_id:
+            self.widget.after_cancel(self.after_id)
+            self.after_id = None
+        self.hide_tooltip()
+
+    def show_tooltip(self):
         if self.tooltip_window or not self.text:
             return
-        x = self.widget.winfo_rootx() + 20
-        y = self.widget.winfo_rooty() + 20
+        x = self.widget.winfo_rootx() + 26
+        y = self.widget.winfo_rooty() + 12
         self.tooltip_window = tw = tk.Toplevel(self.widget)
         tw.wm_overrideredirect(True)
         tw.wm_geometry(f"+{x}+{y}")
@@ -156,13 +167,13 @@ class Tooltip:
         )
         label.pack(ipadx=1)
 
-    def hide_tooltip(self, _):
+    def hide_tooltip(self):
         if self.tooltip_window:
             self.tooltip_window.destroy()
             self.tooltip_window = None
 
 #####################################################################################
-# [] SECTION
+# AUTOCOMPLETE COMBOBOX SECTION
 #####################################################################################
 
 """
